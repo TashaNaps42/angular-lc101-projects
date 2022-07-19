2dc0f7f8f20cef46b8dfec0813c63fc586c2aa84
Tasha L <natasha.lewis.lalor@gmail.com>
2022-07-18 23:35:55 -0500

    Woops let's name it

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Look at all these photos';
+  image1 = 'https://th.bing.com/th/id/OIP.jZiXolTRqRnZY4X-Xm4RZAHaHJ?pid=ImgDet&rs=1';
+  image2 = 'https://th.bing.com/th/id/R.868e9e758421edefcb83d8902fe773a4?rik=Fbmz4wBn02Baqw&pid=ImgRaw&r=0';
+  image3 = 'https://th.bing.com/th/id/OIP.SBaUr87u5JdtSix0bpbvJAHaHS?pid=ImgDet&rs=1';
 
   constructor() { }
 
